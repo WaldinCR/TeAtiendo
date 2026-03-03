4aae4bc6c2818e4cab3bde19206f584243939556
Waldin Ceballos Ramirez <smiwaldin@gmail.com>
2026-03-03 12:20:20 -0400

    logic domain/persistence

--- TeAtiendo.Persistence/Interface/IUnitOfWork.cs
+++ TeAtiendo.Persistence/Interface/IUnitOfWork.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Threading.Tasks;
 using TeAtiendo.Domain.Interfaces;
+using TeAtiendo.Persistence.Context;
+using TeAtiendo.Persistence.Repositories.Operaciones;
+using TeAtiendo.Persistence.Repositories.Seguridad;
 
-namespace TeAtiendo.Persistence.Interfaces
+namespace TeAtiendo.Persistence.Repositories
 {
-    public interface IUnitOfWork : IDisposable
+    public class UnitOfWork : IUnitOfWork
     {
-        IUsuarioRepository Usuarios { get; }
-        IReservaRepository Reservas { get; }
-        IOrdenRepository Ordenes { get; }
-        IPagoRepository Pagos { get; }
+        private readonly TeAtiendoContext _context;
 
-        Task<int> SaveAsync();
+        public IUsuarioRepository Usuarios { get; private set; }
+        public IReservaRepository Reservas { get; private set; }
+        public IOrdenRepository Ordenes { get; private set; }
+        public IPagoRepository Pagos { get; private set; }
+
+        public UnitOfWork(TeAtiendoContext context)
+        {
+            _context = context;
+
+            // Aquí inicializarás los repositorios concretos una vez los tengamos creados
+            Usuarios = new UsuarioRepository(_context);
+            Reservas = new ReservaRepository(_context);
+            Ordenes = new OrdenRepository(_context);
+            Pagos = new PagoRepository(_context);
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
